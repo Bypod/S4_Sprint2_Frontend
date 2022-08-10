c2f4840d75841c745d1e908b6a69e843b1d4285b
Bypod <94805286+Bypod@users.noreply.github.com>
2022-08-10 15:21:53 -0230

    Update App.js

--- auto-finder/src/App.js
+++ auto-finder/src/App.js
@@ -21,10 +21,11 @@ function App() {
     
     
     <Routes>
-      <Route path="" element={<Login setUser={setUser} setIsLoggedIn={setIsLoggedIn} />} />
+      
+      {/* <Route path="" element={<Login setUser={setUser} setIsLoggedIn={setIsLoggedIn} />} />
       <Route path="/register" element={<Register />} />
       
-      <Route path="/search" element={<Search user={user} isLoggedIn={isLoggedIn}/>} /> // this wont be a link but a result of a login
+      <Route path="/search" element={<Search user={user} isLoggedIn={isLoggedIn}/>} /> // this wont be a link but a result of a login */}
     </Routes>
     <Footer />
     </main>
