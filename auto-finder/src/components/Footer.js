3d4adca9796a91030431adc68e1726e4d6b3b916
BFollett21 <bzf421@gmail.com>
2022-08-11 11:34:54 -0230

    Updated how web page looks

--- auto-finder/src/components/Footer.js
+++ auto-finder/src/components/Footer.js
@@ -5,12 +5,13 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
+            <div>
             <hr/><hr/>
             <p>Coded by: Definitly not Stephen Squire</p>
             <p> &copy; Copyright 2022, No Rights Reserved </p>
             <p>Zero credit to Yelp for the sample dataset used for testing</p>
             <p>Not built using create-react-app</p>
-
+            </div>
         </footer>
     )
 }
